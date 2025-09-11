104789ea54e47f5d2bb9083a63b640b4342f9811
Abhinav Tiwari <abhinav.tiwari2_cs23@gla.ac.in>
2025-09-12 00:06:38 +0530

    Time: 4 ms (94.26%), Space: 42.8 MB (92.15%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,10 +1,35 @@
-class Solution {
-    public String reverseWords(String s) {
-        String[] str = s.trim().split("\\s+");
-        String out = "";
-        for (int i = str.length - 1; i > 0; i--) {
-            out += str[i] + " ";
-        }
-        return out + str[0];
+public class Solution {
+  public String reverseWords(String s) {
+    if (s == null) return null;
+    char[] a = s.toCharArray();
+    int n = a.length;
+    reverse(a, 0, n - 1);
+    reverseWords(a, n);
+    return cleanSpaces(a, n);
+  }
+  void reverseWords(char[] a, int n) {
+    int i = 0, j = 0;
+    while (i < n) {
+      while (i < j || i < n && a[i] == ' ') i++; 
+      while (j < i || j < n && a[j] != ' ') j++; 
+      reverse(a, i, j - 1);                     
     }
+  }
+  String cleanSpaces(char[] a, int n) {
+    int i = 0, j = 0;
+    while (j < n) {
+      while (j < n && a[j] == ' ') j++;             
+      while (j < n && a[j] != ' ') a[i++] = a[j++]; 
+      while (j < n && a[j] == ' ') j++;             
+      if (j < n) a[i++] = ' ';                     
+    }
+    return new String(a).substring(0, i);
+  }
+  private void reverse(char[] a, int i, int j) {
+    while (i < j) {
+      char t = a[i];
+      a[i++] = a[j];
+      a[j--] = t;
+    }
+  }
 }
