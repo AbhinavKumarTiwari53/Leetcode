3d7b9516cd8d5cb6fc7cac4e2a2bd5fd454fa430
Abhinav Tiwari <abhinav.tiwari2_cs23@gla.ac.in>
2025-09-12 00:09:41 +0530

    Time: 3 ms (98.99%), Space: 42.8 MB (92.15%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,35 +1,17 @@
-public class Solution {
-  public String reverseWords(String s) {
-    if (s == null) return null;
-    char[] a = s.toCharArray();
-    int n = a.length;
-    reverse(a, 0, n - 1);
-    reverseWords(a, n);
-    return cleanSpaces(a, n);
-  }
-  void reverseWords(char[] a, int n) {
-    int i = 0, j = 0;
-    while (i < n) {
-      while (i < j || i < n && a[i] == ' ') i++; 
-      while (j < i || j < n && a[j] != ' ') j++; 
-      reverse(a, i, j - 1);                     
+class Solution {
+    public String reverseWords(String s) {
+        s = s.trim();
+        int end = s.length();
+        StringBuilder sb = new StringBuilder("");
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i) == ' '){
+                if(i+1<end){
+                    sb.append(s.substring(i+1,end)).append(" ");
+                }
+                end = i;
+            }
+        }
+        sb.append(s.substring(0,end));
+        return sb.toString();
     }
-  }
-  String cleanSpaces(char[] a, int n) {
-    int i = 0, j = 0;
-    while (j < n) {
-      while (j < n && a[j] == ' ') j++;             
-      while (j < n && a[j] != ' ') a[i++] = a[j++]; 
-      while (j < n && a[j] == ' ') j++;             
-      if (j < n) a[i++] = ' ';                     
-    }
-    return new String(a).substring(0, i);
-  }
-  private void reverse(char[] a, int i, int j) {
-    while (i < j) {
-      char t = a[i];
-      a[i++] = a[j];
-      a[j--] = t;
-    }
-  }
 }
