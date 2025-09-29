9e4d6480f68769c0decde9d5921e06e247098cba
Abhinav Tiwari <abhinav.tiwari2_cs23@gla.ac.in>
2025-09-29 14:20:11 +0530

    Time: 136 ms (65.72%), Space: 42.1 MB (25.78%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,27 +1,33 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        int n=board.length, m=board[0].length;
-        int[][] dir={{1,0},{-1,0},{0,1},{0,-1}};
-        int visited[][] = new int[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(board[i][j]==word.charAt(0)){
-                    int k=0;
-                    if(check(board, i, j, dir, k, word,visited)) return true;
+        int n = board.length, m = board[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    int k = 0;
+                    if (check(board, i, j, k, word))
+                        return true;
                 }
             }
         }
         return false;
     }
-    boolean check(char[][] board, int i, int j, int a[][], int k, String word, int[][] visited){
-        if(k>=word.length()) return true;
-        if(i<0 || j<0 || i>=board.length || j>=board[0].length || visited[i][j]==1 || board[i][j]!=word.charAt(k)) return false;
-        visited[i][j]=1;
-        for(int e[]:a){
-            if(check(board,i+e[0],j+e[1],a,k+1,word,visited)) {visited[i][j]=0; return true;}
-        }
-        visited[i][j]=0;
-        return false;
+
+    boolean check(char[][] board, int i, int j, int k, String word) {
+        if (k >= word.length())
+            return true;
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] == '*'
+                || board[i][j] != word.charAt(k))
+            return false;
+        char c = board[i][j];
+        board[i][j] = '*';
+        boolean ans=false;
+        ans |= check(board, i + 1, j, k + 1, word);
+        ans |= check(board, i - 1, j, k + 1, word);
+        ans |= check(board, i , j-1, k + 1, word);
+        ans |= check(board, i , j+1, k + 1, word);
+        board[i][j] = c;
+        return ans;
     }
 
 }
