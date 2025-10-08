0b96b3390130c28d9a399f8b77e1021aa108b565
Abhinav Tiwari <abhinav.tiwari2_cs23@gla.ac.in>
2025-10-08 11:18:43 +0530

    Time: 372 ms (23.95%), Space: 41.3 MB (46.75%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -24,9 +24,9 @@ class Solution {
     }
     boolean can(char[][] board, int row, int col, char c){
         for(int i=0;i<9;i++){
-            if(board[i][col]!='.' && board[i][col]==c) return false;
-            if(board[row][i]!='.' && board[row][i]==c) return false;
-            if(board[3*(row/3)+i/3][3*(col/3)+i%3] != '.' && board[3*(row/3)+i/3][3*(col/3)+i%3] == c) return false;
+            if(board[i][col]==c) return false;
+            if(board[row][i]==c) return false;
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3] == c) return false;
         }
         return true;
     }
