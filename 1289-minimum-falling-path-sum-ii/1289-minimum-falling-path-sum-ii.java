943465ae62481ba8ae02a748a9738facab87326a
Abhinav Tiwari <abhinav.tiwari2_cs23@gla.ac.in>
2025-10-13 22:36:16 +0530

    Time: 35 ms (73.56%), Space: 50.2 MB (82.47%) - LeetHub

---
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minFallingPathSum(int[][] a) {
+        int m=a.length;
+        int n=a[0].length;
+        for(int i=1;i<m;i++){
+            for(int j=0;j<n;j++){
+                int min=Integer.MAX_VALUE;
+                for(int k=0;k<n;k++){
+                    if(k!=j) {
+                        min=Math.min(min, a[i-1][k]);
+                    }
+                }
+                a[i][j]+=min;
+            }
+        }
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            min=Math.min(min,a[m-1][i]);
+        }
+        return min;
+    }
+}
