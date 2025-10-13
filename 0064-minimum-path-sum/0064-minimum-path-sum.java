f9934fae0098e221f5916705eb9c9964dab6b833
Abhinav Tiwari <abhinav.tiwari2_cs23@gla.ac.in>
2025-10-13 16:14:50 +0530

    Time: 4 ms (59.63%), Space: 48.4 MB (8.59%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minPathSum(int[][] a) {
+        int m=a.length;
+        int n=a[0].length;
+        for(int i=1;i<m;i++){
+            a[i][0]+=a[i-1][0];
+        }
+        for(int i=1;i<n;i++){
+            a[0][i]+=a[0][i-1];
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                a[i][j] += Math.min(a[i-1][j],a[i][j-1]);
+            }
+        }
+        return a[m-1][n-1];
+    }
+}
