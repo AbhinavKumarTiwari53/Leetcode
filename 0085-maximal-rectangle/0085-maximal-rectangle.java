e3e4e1a6915063c51844535ae8b2f0502b15e56c
Abhinav Tiwari <abhinav.tiwari2_cs23@gla.ac.in>
2025-11-13 21:06:55 +0530

    Time: 27 ms (10.36%), Space: 50.1 MB (5.09%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -1,15 +1,17 @@
 class Solution {
     public int maximalRectangle(char[][] matrix) {
         int n=matrix.length, m=matrix[0].length, ans=0;
-        for(int i=0;i<n ;i++){
-            int r[]=new int[m];
-            for(int j=i; j<n;j++){
-                int h=j-i+1, sum=0;
-                for(int k=0; k<m;k++){
+        for(int i=0;i<n;i++){
+            int r[] = new int[m];
+            for(int j=i;j<n;j++){
+                for(int k=0;k<m;k++){
                     r[k]+=matrix[j][k]-'0';
+                }
+                int h=j-i+1, sum=0;
+                for(int k=0;k<m;k++){
                     if(r[k]==h){
                         sum+=h;
-                        ans=Math.max(ans,sum);
+                        ans=Math.max(ans, sum);
                     }
                     else sum=0;
                 }
