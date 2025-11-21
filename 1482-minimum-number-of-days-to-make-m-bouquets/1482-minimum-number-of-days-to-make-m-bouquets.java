d37a5b34d4f0ae91d80da3ae9dd0607d17a74dac
Abhinav Tiwari <abhinav.tiwari2_cs23@gla.ac.in>
2025-11-21 11:02:42 +0530

    Time: 19 ms (52.52%), Space: 82.5 MB (22.65%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,16 +1,26 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
-        int n = bloomDay.length,l=1,r=(int)1e9;
-        if((long)m*k>n) return -1;
-        while(l<r){
-            int mid = (l+r)/2, f=0,bq=0;
-            for(int j =0;j<n;++j){
-                if(bloomDay[j] >mid) f=0;
-                else if(++f>=k) {bq++; f=0;}
-            }
-            if(bq<m) l=mid+1;
-            else r=mid;
+        if(((long)m*k) > bloomDay.length) return -1;
+        int l=1, r=Arrays.stream(bloomDay).max().getAsInt();
+        int ans=Integer.MAX_VALUE;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            int b=isPossible(bloomDay,mid, k);
+            if(b>=m) {ans=mid; r=mid-1;}
+            else l=mid+1;
         }
-        return l;
+        return ans;
+    }
+    int isPossible(int[] a, int mid, int k) {
+        int total=0;
+        int c=0;
+        for(int i:a){
+            if(i<=mid) {
+                c++;
+                if(c==k) {total++; c=0;}
+            }
+            else c=0;
+        }
+        return total;
     }
 }
