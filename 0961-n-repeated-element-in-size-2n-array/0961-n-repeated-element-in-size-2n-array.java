07580dd64636f62fa7587b24f0c8491fc9fcc5b8
Abhinav Tiwari <abhinav.tiwari2_cs23@gla.ac.in>
2026-01-02 10:36:52 +0530

    Time: 3 ms (48.08%), Space: 47.1 MB (82.11%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -1,7 +1,11 @@
 class Solution {
     public int repeatedNTimes(int[] nums) {
-        Arrays.sort(nums);
         int n=nums.length;
-        return nums[n/2]==nums[n-1]?nums[n/2]:nums[n/2-1];
+        int nu[] = new int[10000];
+        for(int i=0;i<n;i++){
+            nu[nums[i]]++;
+            if(nu[nums[i]]==n/2) return nums[i];
+        }
+        return -1;
     }
 }
