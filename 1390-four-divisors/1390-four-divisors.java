cd92e6b3198c08fcadcb58ff6b6bf5d8a6e319d8
Abhinav Tiwari <abhinav.tiwari2_cs23@gla.ac.in>
2026-01-04 10:53:16 +0530

    Time: 17 ms (56.31%), Space: 46.5 MB (33.08%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.java
+++ 1390-four-divisors/1390-four-divisors.java
@@ -4,8 +4,12 @@ class Solution {
         for(int i:nums) {
             int c=0;
             int sum=0;
-            for(int j=1;j<=i;j++){
-                if(i%j==0) {c++; sum+=j;}
+            for(int j=1;j<=Math.sqrt(i);j++){
+                if(i%j==0) {
+                    c++;
+                    sum+=j;
+                    if(j!=i/j) {c++; sum+=i/j;}
+                }
                 if(c>4) {sum=0; break;}
             }
             ans+=(c==4)?sum:0;
