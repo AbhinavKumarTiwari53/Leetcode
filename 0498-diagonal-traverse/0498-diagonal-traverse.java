a2f46bc20f42836de0102d44d7afc681204407bc
Abhinav Tiwari <abhinav.tiwari2_cs23@gla.ac.in>
2026-01-08 19:30:10 +0530

    Time: 3 ms (74.28%), Space: 48.4 MB (9.55%) - LeetHub

---
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] findDiagonalOrder(int[][] mat) {
+        int x=0, y=0, rows=mat.length, columns=mat[0].length;
+        int[] result=new int[rows * columns];
+        for(int i=0; i<result.length; i++){
+            result[i]=mat[x][y];
+            if((x+y)%2==0){ 
+                if(y==columns-1){
+                    x++;
+                }else if(x==0){
+                    y++;
+                }else{
+                    x--;
+                    y++;
+                }
+            }else{
+                if(x==rows-1){
+                    y++;
+                }else if(y==0){
+                    x++;
+                }else{
+                    x++;
+                    y--;
+                }                
+            }
+        }
+    return result;    
+    }
+}
