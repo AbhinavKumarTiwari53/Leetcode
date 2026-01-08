37bdd3cded3b32440dbd29400b1de49a766172a6
Abhinav Tiwari <abhinav.tiwari2_cs23@gla.ac.in>
2026-01-08 19:09:24 +0530

    Time: 0 ms (100%), Space: 48.2 MB (58.02%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int n = s.length, i=0;
+        int j = n - 1;
+        while ( i < j ) {
+            char t = s[i];
+            s[i] = s[j];
+            s[j] = t;
+            i++; j--;
+        }
+    }
+}
