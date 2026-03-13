49ce0d69a85b2b281904112e63312a7714aa40c3
Abhinav Tiwari <abhinav.tiwari2_cs23@gla.ac.in>
2026-03-13 12:18:50 +0530

    Time: 0 ms (100%), Space: 47.6 MB (52.73%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -1,44 +1,20 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    int e=-1;
     public TreeNode deleteNode(TreeNode root, int key) {
-        return del(root,key);
-    }
-    public TreeNode del(TreeNode root, int key){
-        if(root == null) return null;
-        if(root.val == key){
-            if(root.left == null && root.right==null) return null;
-            if(root.left == null || root.right==null) return (root.left==null)?root.right:root.left;
-            if(root.left!=null && root.right!=null){
-                root.left=f(root.left);
-                root.val=e;
-                return root;
-            }
+        if(root==null) return null;
+        if(key < root.val) root.left=deleteNode(root.left,key);
+        else if(key > root.val) root.right=deleteNode(root.right,key);
+        else {
+            if(root.left == null) return root.right;
+            if(root.right == null) return root.left;
+            TreeNode s=findMin(root.right);
+            root.val=s.val;
+            root.right=deleteNode(root.right,s.val); 
         }
-        if(root.val<key) root.right=del(root.right,key);
-        if(root.val>key) root.left=del(root.left,key);
         return root;
     }
-    public TreeNode f(TreeNode root){
-        if(root.right==null){
-            e=root.val;
-            return root.left;
-        }
-        root.right=f(root.right);
-        return root;
+
+    TreeNode findMin(TreeNode node){
+        while(node.left!=null) node=node.left;
+        return node;
     }
 }
