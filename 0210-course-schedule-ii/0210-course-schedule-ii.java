db7ffa18226f06f505c5afabf0695db34686cbe3
Abhinav Tiwari <abhinav.tiwari2_cs23@gla.ac.in>
2026-03-17 16:01:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,37 +1,23 @@
 class Solution {
-    public int[] findOrder(int n, int[][] prerequisites) {
-        List<Integer>[] adj = new List[n];
-        int[] indegree = new int[n];
-        int[] ans = new int[n];
-
-        for (int[] pair : prerequisites) {
-            int course = pair[0];
-            int prerequisite = pair[1];
-            if (adj[prerequisite] == null) {
-                adj[prerequisite] = new ArrayList<>();
-            }
-            adj[prerequisite].add(course);
-            indegree[course]++;
+    public int[] findOrder(int n, int[][] pre) {
+        List<List<Integer>> li=new ArrayList<>();
+        int[] in=new int[n];
+        int[] ans=new int[n];
+        for(int i=0;i<n;i++) li.add(new ArrayList<>());
+        for(int i[]:pre){
+            li.get(i[1]).add(i[0]);
+            in[i[0]]++;
         }
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<n;i++) if(in[i]==0) q.add(i);
 
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) {
-                queue.offer(i);
-            }
-        }
         int i=0;
-        while (!queue.isEmpty()) {
-            int current = queue.poll();
-            ans[i++]=current;
-
-            if (adj[current] != null) {
-                for (int next : adj[current]) {
-                    indegree[next]--;
-                    if (indegree[next] == 0) {
-                        queue.offer(next);
-                    }
-                }
+        while(!q.isEmpty()){
+            int a=q.poll();
+            ans[i++]=a;
+            for(int j:li.get(a)){
+                in[j]--;
+                if(in[j]==0) q.add(j);
             }
         }
         if(i!=n) return new int[0];
