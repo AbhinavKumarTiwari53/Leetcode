6e0d51da670c5deb71c1efabf8eff322f76e744e
Abhinav Tiwari <abhinav.tiwari2_cs23@gla.ac.in>
2026-03-17 11:48:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,43 +1,30 @@
 class Solution {
-    public int minimumEffortPath(int[][] heights) {
-        
-        int n=heights.length;
-        int m=heights[0].length;
-        
-        if(n==1 && m==1)
-            return 0;
-        
-        int efforts[][]=new int[n][m];//stores the minimum effots required to travel upto a given cell
-        for(int row[]:efforts)
-            Arrays.fill(row,Integer.MAX_VALUE);
-        
-        PriorityQueue<int[]>pq=new PriorityQueue<int[]>((t1,t2)->(t1[2]-t2[2]));//returns cell with minimum efforts
-        
+    public int minimumEffortPath(int[][] h) {
+        int n=h.length, m=h[0].length;
+        if(n==1 && m==1) return 0;
+        int[][] effort = new int[n][m];
+        for(int i[]:effort){
+            Arrays.fill(i,Integer.MAX_VALUE);
+        }
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[2]-b[2]);
         pq.offer(new int[]{0,0,0});
-        int dir[][]={{0,1},{0,-1},{1,0},{-1,0}};//direction to travel
-        
-        while(!pq.isEmpty())
-        {
-            int curr[]=pq.poll();
-            int curr_row=curr[0];
-            int curr_col=curr[1];
-            int curr_wt=curr[2];
-            for(int x[]:dir)
-            {
-                int nrow=curr_row+x[0];
-                int ncol=curr_col+x[1];
-                if(nrow<0 || nrow>=n || ncol<0 || ncol>=m)
+        int[][] dir={{0,1},{0,-1},{1,0},{-1,0}};
+        while(!pq.isEmpty()){
+            int[] cur=pq.poll();
+            int r=cur[0],c=cur[1];
+            int ce=cur[2];
+            for(int d[]:dir){
+                int nr=r+d[0];
+                int nc=c+d[1];
+                if(nr<0 || nr>=n || nc<0 || nc>=m)
                     continue;
-                int wt = Math.max(curr_wt,Math.abs(heights[nrow][ncol]-heights[curr_row][curr_col]));//getting max absolute value
-                //updating the minimum effort
-                if(wt<efforts[nrow][ncol])
-                {
-                    efforts[nrow][ncol]=wt;
-                    pq.offer(new int[]{nrow,ncol,wt});
+                int ne=Math.max(ce,Math.abs(h[nr][nc]-h[r][c]));
+                if(ne<effort[nr][nc]){ 
+                    effort[nr][nc]=ne;
+                    pq.offer(new int[]{nr,nc,ne});
                 }
-                
             }
         }
-        return efforts[n-1][m-1];
+        return effort[n-1][m-1];
     }
 }
