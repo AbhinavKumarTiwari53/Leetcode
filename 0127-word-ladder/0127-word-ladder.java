ab2e2d9b11e29cb2bbf851e2d11dcdc2afd26b05
Abhinav Tiwari <abhinav.tiwari2_cs23@gla.ac.in>
2026-03-17 11:57:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,31 +1,31 @@
 class Solution {
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+    public int ladderLength(String f, String l, List<String> wordList) {
         Set<String> set=new HashSet<>();
         for(String s:wordList) set.add(s);
-        if(!set.contains(endWord)) return 0;
-        int l=1;
+        if(!set.contains(l)) return 0;
         Queue<String> q=new LinkedList<>();
-        q.add(beginWord);
+        q.add(f);
+        int lvl=1;
         while(!q.isEmpty()){
             int size=q.size();
             for(int i=0;i<size;i++){
                 String s=q.poll();
-                char cc[]=s.toCharArray();
-                for(int j=0;j<cc.length;j++){
-                    char f=cc[j];
-                    for(char ch='a';ch<='z';ch++){
-                        cc[j]=ch;
-                        String temp=new String(cc);
-                        if(temp.equals(endWord)) return l+1;
+                char c[] = s.toCharArray();
+                for(int j=0;j<c.length;j++){
+                    char ff=c[j];
+                    for(char ch='a';ch<='z'; ch++){
+                        c[j]=ch;
+                        String temp=new String(c);
+                        if(temp.equals(l)) return lvl+1;
                         if(set.contains(temp)){
                             q.add(temp);
                             set.remove(temp);
                         }
                     }
-                    cc[j]=f;
+                    c[j]=ff;
                 }
             }
-            l++;
+            lvl++;
         }
         return 0;
     }
