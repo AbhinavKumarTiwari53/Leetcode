c48249ba57685b6989d3837703198e16624277dd
Abhinav Tiwari <abhinav.tiwari2_cs23@gla.ac.in>
2026-04-01 15:18:54 +0530

    Time: 1 ms (99.98%), Space: 47 MB (56.87%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ans = 0;
-        for(int i : nums) ans ^= i;
-        return ans;
+        int a=0;
+        for(int i:nums) a^=i;
+        return a;
     }
 }
