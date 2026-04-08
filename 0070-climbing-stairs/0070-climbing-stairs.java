5191f91e5a646f9613f411ea44e121437508c14a
Abhinav Tiwari <abhinav.tiwari2_cs23@gla.ac.in>
2026-04-08 15:47:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,11 +1,14 @@
 class Solution {
     public int climbStairs(int n) {
-        int dp[]=new int[n+1];
-        dp[0]=1;
-        dp[1]=2;
-        for(int i=2;i<n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+        if(n<3) return n;
+        int pp=1;
+        int p=2;
+        int ans=0;
+        for(int i=3;i<=n;i++){
+            ans=pp+p;
+            pp=p;
+            p=ans;
         } 
-        return dp[n-1];
+        return ans;
     }
 }
