93ef90ab0386760fe115f10704803a12a71e2073
Abhinav Tiwari <abhinav.tiwari2_cs23@gla.ac.in>
2026-04-08 15:54:11 +0530

    Time: 2 ms (100%), Space: 110.6 MB (88.86%) - LeetHub

---
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+        public int climbStairs(int n, int[] costs) {
+        int v0=0,v1=0,v2=0,v;
+        for(int c:costs){
+            v=Math.min(Math.min(v0+9,v1+4),v2+1)+c;
+            v0=v1;
+            v1=v2;
+            v2=v;
+        }
+        return v2;
+    }
+}
