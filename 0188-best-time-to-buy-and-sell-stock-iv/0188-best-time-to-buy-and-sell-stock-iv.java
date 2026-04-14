b6cb7615e378a427bddf739782a31bc329c5fa93
Abhinav Tiwari <abhinav.tiwari2_cs23@gla.ac.in>
2026-04-14 12:46:54 +0530

    Time: 0 ms (100%), Space: 42.9 MB (97.68%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,21 +1,16 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
-        Integer[][][] dp=new Integer[prices.length][2][k+1];
-        return help(prices,0,1,k,dp);
-    }
-    int help(int[] arr, int i,int can , int cap,Integer dp[][][]){
-        if(cap==0 || i==arr.length) return 0;
-        if(dp[i][can][cap]!=null) return dp[i][can][cap];
-        int pro=0;
-        if(can==1){
-            int buy=-arr[i] + help(arr,i+1,0,cap,dp);
-            int skip=help(arr,i+1,1,cap,dp);
-            pro=Math.max(buy,skip);
-        }else {
-            int sell=arr[i]+help(arr,i+1,1,cap-1,dp);
-            int skip=help(arr,i+1,0,cap,dp);
-            pro=Math.max(sell,skip);
+        int n = prices.length;
+        int[] profits = new int[n];
+        for (int i = 1; i <= k ; i++) {
+            int max = 0;
+            int profit = 0;
+            for( int j = n-1; j >= 0; j--) {
+                max = Math.max(max, profits[j]+prices[j]);
+                profit = Math.max(profit, max-prices[j]);
+                profits[j] = profit;
+            }
         }
-        return dp[i][can][cap]=pro;
+        return profits[0];
     }
 }
